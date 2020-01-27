9f2725da17e77f6b79138e0e2f24343fc6f1e844
Philipotieno <tmitchellb007@gmail.com>
2020-01-27 14:16:10 +0300

    Feat: create simple react componets

---
+++ src/components/create-student.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class CreateStudent extends Component {
+    render() {
+        return (
+            <div>
+                <p>React create student Component</p>
+            </div>
+        );
+    }
+}
+
+export default CreateStudent;
---
+++ src/components/student-list.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ListStudent extends Component {
+    render() {
+        return (
+            <div>
+                <p>React list student Component</p>
+            </div>
+        );
+    }
+}
+
+export default ListStudent;
