2c44aed8d1545eea146d6d0104d5b4b843cf2b27
Philipotieno <tmitchellb007@gmail.com>
2020-01-27 15:45:34 +0300

    Feat: move files to frontend folder

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Nav from "react-bootstrap/Nav";
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
