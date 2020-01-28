aa289a024899134fcd48af5eb275698dd86ce593
Philipotieno <tmitchellb007@gmail.com>
2020-01-28 13:48:16 +0300

    rename frontend folder

---
+++ frontend/src/components/edit-student.component.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class EditStudent extends Component {
+    render() {
+        return (
+            <div>
+                <p>React edit student Component</p>
+            </div>
+        );
+    }
+}
+
+export default EditStudent;
